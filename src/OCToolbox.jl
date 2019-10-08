a82aefd0ad8226aaccb7a7abb78cd8393d9e68cb
Thomas <t.reisser@fz-juelich.de>
2019-10-08 02:23:21 +0200

    Added some functions, modules and testing for the Frechet exponential using PyCall and the Python implementation of the Fechet exponential

--- src/OCToolbox.jl
+++ src/OCToolbox.jl
@@ -1,5 +1,7 @@
 module OCToolbox
 
+include("./Frechet_Exponential_Methods/Frechet_Exponential.jl")
+
 greet() = print("Hello World!")
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
 using OCToolbox
 using Test
 
-@testset "OCToolbox.jl" begin
-    # Write your own tests here.
+@testset "Frechet_Exponential.jl" begin
+    M = [log(1) 0 0; 0 log(1) 0; 0 0 log(1)]
+    @test OCToolbox.expm_frechet(M, M) ==
+    ([1.0 0.0 0.0; 0.0 1.0 0.0; 0.0 0.0 1.0],
+    [0.0 0.0 0.0; 0.0 0.0 0.0; 0.0 0.0 0.0])
+    # implement more and better tests here!!!
 end
