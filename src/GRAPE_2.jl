07b45f049ef39a9ff0f3da65ff7c73fe2ad50ea3
Alastair <alastair@nvision-imaging.com>
2020-01-07 14:47:29 +0100

    GRAPE as a function

---
+++ src/GRAPE_2.jl
@@ -0,0 +1,58 @@
+using Zygote
+using QuantumOpticsBase
+using Optim
+
+# so the idea here is to set up a function that does everything and takes a
+# functional as input
+
+function GRAPE(functional, controls, K, N)
+    # autodiff the given functional
+    # assume that everything is always flat
+
+    function ∇(x)
+        y, back = Zygote.pullback(functional, x)
+        grady = back(1)[1]
+        return reshape(grady, (K*N))
+    end
+
+    f = x -> functional(reshape(x, (K, N)))
+    g = x -> ∇(reshape(x, (K, N)))
+
+    res = optimize(f, g, controls, LBFGS(); inplace = false)
+    return res.minimizer
+end
+
+
+b = SpinBasis(1//2)
+const sx = DenseOperator(sigmax(b)).data
+const sy = DenseOperator(sigmay(b)).data
+const sz = DenseOperator(sigmaz(b)).data
+const i2 = DenseOperator(identity(b)).data
+
+ρ0 = [1 + 0.0im, 0.0+0.0im]
+# target state
+σ0 = [0.0 + 0.0im, 1.0 + 0.0im]
+
+# hardcoded propagator for the moment, need to fix this at some point...
+propagator(c_vec) = exp((c_vec[1] .* sx .+ c_vec[2] .* sy) .* (-Δt*π*im))
+
+function J3(x)
+    x = complex.(real.(x))
+    test_forward = mapreduce(propagator, *, eachcol(x))
+    1 - abs2(σ0' * test_forward * ρ0)
+end
+
+N = 5 # 5 steps
+K = 2 # 2 controls
+
+T = 1
+Δt = T/N
+
+x = 1/1000 * (rand(K, N) .* 2 .- 1)
+x = x * 0 .+ 1/sqrt(2)/2
+J3(x)
+
+
+z = GRAPE(J3, x, K, N)
+
+J3(z)
