b7adbebece436662d5b01819424a5e964a103727
Alastair <alastair@nvision-imaging.com>
2020-02-19 16:36:51 +0100

    Fixed a bug in state transfer, added an example of using the cost functions!

--- src/tools.jl
+++ src/tools.jl
@@ -8,12 +8,12 @@ These include export functions for pulses an plotting functions
 """
 function export_pulse(filename, pulse)
     open(filename, "w") do io
-        writedlm(io, pulse.T pulse.Ω pulse.ϕ)
+        writedlm(io, [pulse.T pulse.Ω pulse.ϕ])
     end
 end
 
 function export_pulse(filename, T, Ω, ϕ)
     open(filename, "w") do io
-        writedlm(io, T Ω ϕ)
+        writedlm(io, [T Ω ϕ])
     end
 end
